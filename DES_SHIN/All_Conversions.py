55421ba67f32af28624c68ad53708ed4797e243a
cdzer0 <53449238+cdzer0@users.noreply.github.com>
2021-02-13 15:54:44 +0530

    Add files via upload

---
+++ DES_SHIN/All_Conversions.py
@@ -0,0 +1,97 @@
+# Hexadecimal to binary conversion 
+def hex2bin(s): 
+	mp = {'0' : "0000", 
+		'1' : "0001", 
+		'2' : "0010", 
+		'3' : "0011", 
+		'4' : "0100", 
+		'5' : "0101", 
+		'6' : "0110", 
+		'7' : "0111", 
+		'8' : "1000", 
+		'9' : "1001", 
+		'a' : "1010", 
+		'b' : "1011", 
+		'c' : "1100", 
+		'd' : "1101", 
+		'e' : "1110", 
+		'f' : "1111" } 
+	bin = "" 
+	for i in range(len(s)): 
+		bin = bin + mp[s[i]] 
+	return bin
+	
+# Binary to hexadecimal conversion 
+def bin2hex(s): 
+	mp = {"0000" : '0', 
+		"0001" : '1', 
+		"0010" : '2', 
+		"0011" : '3', 
+		"0100" : '4', 
+		"0101" : '5', 
+		"0110" : '6', 
+		"0111" : '7', 
+		"1000" : '8', 
+		"1001" : '9', 
+		"1010" : 'a', 
+		"1011" : 'b', 
+		"1100" : 'c', 
+		"1101" : 'd', 
+		"1110" : 'e', 
+		"1111" : 'f' } 
+	return mp[s]
+
+# Binary to decimal conversion 
+def bin2dec(binary): 
+	binary1 = binary 
+	decimal, i, n = 0, 0, 0
+	while(binary != 0): 
+		dec = binary % 10
+		decimal = decimal + dec * pow(2, i) 
+		binary = binary//10
+		i += 1
+	return decimal 
+
+# Decimal to binary conversion 
+def dec2bin(num): 
+	res = bin(num).replace("0b", "") 
+	if(len(res)%4 != 0): 
+		div = len(res) / 4
+		div = int(div) 
+		counter =(4 * (div + 1)) - len(res) 
+		for i in range(0, counter): 
+			res = '0' + res 
+	return res
+
+def pt2hex(message, retdict):
+	t1 = ""
+	if len(message) % 8 != 0:
+		extra = len(message)%8
+		while extra:
+			message += "0"
+			extra -= 1
+	for i in range(len(message)):
+		num = ord(message[i])
+		t1 += str(num) + " "
+	#print(t1)
+	t2 = ""
+	pnum = ""
+	for l in t1:
+		if l == " ":
+			num = dec2bin(int(pnum))
+			pnum = ""
+			t2 += str(num)
+		else:
+			pnum += l  
+	#print(t2)
+	retdict['b'] = t2
+	t3 = ""
+	for i in range(0,len(t2),4):
+		ch = "" 
+		ch = ch + t2[i] 
+		ch = ch + t2[i + 1] 
+		ch = ch + t2[i + 2] 
+		ch = ch + t2[i + 3]
+		t3 += bin2hex(ch)
+	#print(t3)
+	retdict['h'] = t3
---
+++ DES_SHIN/DES.py
@@ -0,0 +1,156 @@
+import All_Conversions as ac
+import All_Functions as af
+
+key = "e1287a90b6cf35d4" #Some hex value
+PlainText = "Hi this is trial and hope this works"
+CipherText = ""
+
+returned_pt_val = {}
+returned_key_val = {}
+returned_block_val = {}
+key_dict = {}
+ac.pt2hex(PlainText, returned_pt_val)
+
+bin_key = ac.hex2bin(key)
+p1key = af.permuteOne(bin_key) # 64-bit -> 56-bit key
+
+# def Testing():
+#     # ******** TESTING FOR KEY ******** #
+#     test_bin_key = "0001001100110100010101110111100110011011101111001101111111110001"
+#     p1key = af.permuteOne(test_bin_key) # 64-bit -> 56-bit key
+#     print("p1key is : " + p1key + " Its length is " + str(len(p1key)))
+
+#     # ******** TESTING FOR BLOCK ******** #
+#     test_block = "0000000100100011010001010110011110001001101010111100110111101111"
+#     test_block_list = test_block[:]
+#     test_block_str = af.permuteThree(test_block_list)
+#     print("Final Message is " + test_block_str)
+#     # R0 = "11110000101010101111000010101010"
+#     # IP = "1100110000000000110011001111111111110000101010101111000010101010"
+#     test_returned_block_val = {}
+#     final_test_block_str = ""
+#     inv_final_test_block_str = ""
+#     for j in range(16):
+#         print("#ROUND " + str(j+1) + " : ")
+#         af.keyRotationNpermuteTwo(p1key,j,returned_key_val) 
+#         p1key = returned_key_val["lcs_key"]
+#         print("p1key is : " + p1key + " Its length is " + str(len(p1key)))
+#         key_dict[j] =  returned_key_val["p2key"]
+
+#         af.encrypt(test_block_str, returned_key_val["p2key"],test_returned_block_val)
+#         test_block_str = test_returned_block_val['l'] + test_returned_block_val['r']
+#         print("test_block_str is : " + test_block_str)
+#         print("final_r_block is : " + test_returned_block_val['r'])
+#         print("final_l_block is : " + test_returned_block_val['l'])
+#         print("#ROUND " + str(j+1) + " Ended. \n")
+
+#     final_test_block_str = test_returned_block_val['r'] + test_returned_block_val['l']
+#     print("final_test_block_str is : " + final_test_block_str)
+#     inv_final_test_block_str = af.permuteFive(final_test_block_str)
+#     print("inv_final_test_block_str is : " + inv_final_test_block_str)
+
+#     CT = ""
+#     for i in range(0,len(inv_final_test_block_str),4):
+#         ch = "" 
+#         ch = ch + inv_final_test_block_str[i] 
+#         ch = ch + inv_final_test_block_str[i + 1] 
+#         ch = ch + inv_final_test_block_str[i + 2] 
+#         ch = ch + inv_final_test_block_str[i + 3]
+#         CT += ac.bin2hex(ch)
+#     print("CT is : " + CT)
+
+# Testing()
+
+
+
+#******** START OF ENCRYPTION ********#
+for i in range(0, len(returned_pt_val['b']), 64):
+    block = returned_pt_val['b'][i:i+64]
+    #print("Block " + str(int(i/64) + 1) + " is : " + block)
+    block_str = af.permuteThree(block)
+    final_block_str = ""
+    inv_final_block_str = ""
+
+    #******** START OF ROUNDS ON EACH BLOCK ********#
+    for j in range(16):
+        af.keyRotationNpermuteTwo(p1key,j,returned_key_val) 
+        p1key = returned_key_val["lcs_key"]
+
+        #print(block_str)
+
+        key_dict[j] =  returned_key_val["p2key"]
+        af.encrypt(block_str, returned_key_val["p2key"],returned_block_val)
+        block_str = returned_block_val['l'] + returned_block_val['r']
+        #print("block_str is : " + block_str)
+        #print("final_r_block is : " + returned_block_val['r'])
+        #print("final_l_block is : " + returned_block_val['l'])
+        #print("#ROUND " + str(j+1) + " Ended. \n")
+
+    final_block_str = returned_block_val['r'] + returned_block_val['l']
+    #print("final_block_str is : " + final_block_str)
+    inv_final_block_str = af.permuteFive(final_block_str)
+    CT = ""
+    for i in range(0,len(inv_final_block_str),4):
+        ch = "" 
+        ch = ch + inv_final_block_str[i] 
+        ch = ch + inv_final_block_str[i + 1] 
+        ch = ch + inv_final_block_str[i + 2] 
+        ch = ch + inv_final_block_str[i + 3]
+        CT += ac.bin2hex(ch)
+    #print("CT for " + inv_final_block_str + " is : " + CT + "\n")
+    CipherText += CT
+print("\nCipherText is : " + CipherText + "\n")
+
+for i in range(16):
+    print("Key for round " + str(i) + " is : " +  key_dict[i])
+
+#******** DECRYPTION ********#
+d_returned_key_val = {}
+d_returned_block_val = {}
+
+p1key = af.permuteOne(bin_key)
+d_block = ""
+RPlainText = ""
+
+#******** START OF DECRYPTION ********#
+for i in range(0, len(CipherText), 16):
+    d_block = CipherText[i:i+16]
+    print("D_Block " + str(int(i/16) + 1) + " is : " + d_block)
+    bin_d_block = ""
+    for i in range(len(d_block)):
+        bin_d_block += ac.hex2bin(d_block[i])
+    print("bin_d_block for D_Block " + d_block + " is : " + bin_d_block)
+    bin_d_block_str = af.permuteThree(bin_d_block)
+    print("bin_d_block_str is : " + bin_d_block_str)
+    d_final_block_str = ""
+    d_inv_final_block_str = ""
+
+    #******** START OF ROUNDS ON EACH BIN_D_BLOCK ********#
+    for j in range(16):
+        # af.keyRotationNpermuteTwo(p1key,j,d_returned_key_val) 
+        # p1key = d_returned_key_val["lcs_key"]
+
+        #print(bin_d_block_str)
+
+        af.encrypt(bin_d_block_str, key_dict[15-j], d_returned_block_val) # d_returned_key_val["p2key"]
+        bin_d_block_str = d_returned_block_val['l'] + d_returned_block_val['r']
+        #print("block_str is : " + block_str)
+        #print("final_r_block is : " + returned_block_val['r'])
+        #print("final_l_block is : " + returned_block_val['l'])
+        #print("#ROUND " + str(j+1) + " Ended. \n")
+
+    d_final_block_str = d_returned_block_val['r'] + d_returned_block_val['l']
+    print("d_final_block_str is : " + d_final_block_str)
+    d_inv_final_block_str = af.permuteFive(d_final_block_str)
+    print("d_inv_final_block_str is : " + d_inv_final_block_str)
+
+    PT = ""
+    for a in range(0,len(d_inv_final_block_str),8):
+        ch = d_inv_final_block_str[a:a+8]
+        print("ch is : " + ch)
+        num = ac.bin2dec(int(ch))
+        print("num is : " + str(num))
+        PT += chr(num)
+    print("PT for " + d_inv_final_block_str + " is : " + PT + "\n")
+    RPlainText += PT
+print("\nRecovered PlainText is : " + RPlainText + "\n")
