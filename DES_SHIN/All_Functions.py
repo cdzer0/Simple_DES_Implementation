564a305a0ea5d570c71e8b8db0dd1f32c813f2aa
cdzer0 <53449238+cdzer0@users.noreply.github.com>
2021-02-23 20:19:18 +0530

    Update All_Functions.py
    Generates a random key everytime

--- DES_SHIN/All_Functions.py
+++ DES_SHIN/All_Functions.py
@@ -1,4 +1,13 @@
 import All_Conversions as ac
+import random
+
+# Random Key Generation
+def GenerateKey():
+    key_base = "0123456789abcdef"
+    key = ""
+    for i in range(16):
+        key += random.choice(key_base)
+    return key
 
 # Key Initial Permutation 1 
 IP1 = [56, 48, 40, 32, 24, 16, 8, 0, 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 60, 52, 44, 36, 28, 20, 12, 4, 27, 19, 11, 3]
